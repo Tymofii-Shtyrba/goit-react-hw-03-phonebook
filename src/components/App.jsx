ae5dad38ff7a7a4f2b8efba47b54d82957f547ac
Tymofii-Shtyrba <timofeyshtyrba@gmail.com>
2023-05-06 02:19:13 +0300

    Added the local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,6 +41,19 @@ export class App extends Component {
     });
   };
 
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
+
+  componentDidMount = () => {
+    const savedList = JSON.parse(localStorage.getItem('contacts'));
+    if (savedList) {
+      this.setState({ contacts: savedList });
+    }
+  };
+
   render() {
     const filteredContacts = this.handleFilterContacts();
     return (
